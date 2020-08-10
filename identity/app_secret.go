99aafd8b9c801e2779b9618bd95600f37e6556f6
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-08-10 16:16:00 +0200

    feat(identity): Expose AppSecretSize and AppPublicKeySize constants

--- identity/app_secret.go
+++ identity/app_secret.go
@@ -4,13 +4,13 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
-const appSecretSize = 64
-const appPublicKeySize = 32
+const AppSecretSize = 64
+const AppPublicKeySize = 32
 const authorSize = 32
 const appCreationNature = 1
 
 func generateAppID(appSecret []byte) []byte {
-	publicKey := appSecret[appSecretSize-appPublicKeySize : appSecretSize]
+	publicKey := appSecret[AppSecretSize-AppPublicKeySize : AppSecretSize]
 	author := make([]byte, 32)
 	payload := append([]byte{appCreationNature}, author...)
 	payload = append(payload, publicKey...)
