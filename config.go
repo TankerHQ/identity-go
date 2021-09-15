ec6594a19bea959adae6b9aab399ef912683ccbd
Michel Levieux <michel.levieux@tanker.io>
2021-09-15 10:32:46 +0200

    more specific error messages on wrong key-size

--- config.go
+++ config.go
@@ -27,7 +27,7 @@ func (cfg Config) fromBase64() (config, error) {
 		return config{}, fmt.Errorf("unable to decode AppID '%s', should be a valid base64 string", cfg.AppID)
 	}
 	if len(newCfg.AppID) != AppPublicKeySize {
-		return config{}, fmt.Errorf("wrong size for AppID: %d, should be %d", len(newCfg.AppID), AppPublicKeySize)
+		return config{}, fmt.Errorf("wrong byte size for AppID: %d, should be %d", len(newCfg.AppID), AppPublicKeySize)
 	}
 
 	newCfg.AppSecret, err = base64.StdEncoding.DecodeString(cfg.AppSecret)
@@ -35,7 +35,7 @@ func (cfg Config) fromBase64() (config, error) {
 		return config{}, fmt.Errorf("unable to decode AppSecret '%s', should be a valid base64 string", cfg.AppSecret)
 	}
 	if len(newCfg.AppSecret) != AppSecretSize {
-		return config{}, fmt.Errorf("wrong size for AppSecret: %d, should be %d", len(newCfg.AppSecret), AppSecretSize)
+		return config{}, fmt.Errorf("wrong byte size for AppSecret: %d, should be %d", len(newCfg.AppSecret), AppSecretSize)
 	}
 
 	return newCfg, nil
