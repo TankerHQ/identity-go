3cf0f8949febd3cd956bf31eef37e643d7fdd0ad
Michel Levieux <michel.levieux@tanker.io>
2021-09-20 10:57:38 +0200

    function renaming in config and app_secret

--- config.go
+++ config.go
@@ -15,17 +15,17 @@ type config struct {
 	AppSecret []byte
 }
 
-func (this Config) fromB64() (*config, error) {
-	appIDBytes, err := base64.StdEncoding.DecodeString(this.AppID)
+func (cfg Config) fromBase64() (*config, error) {
+	appIDBytes, err := base64.StdEncoding.DecodeString(cfg.AppID)
 	if err != nil {
-		return nil, fmt.Errorf("unable to decode AppID '%s', should be a valid base64 string", this.AppID)
+		return nil, fmt.Errorf("unable to decode AppID '%s', should be a valid base64 string", cfg.AppID)
 	}
 	if len(appIDBytes) != AppPublicKeySize {
 		return nil, fmt.Errorf("wrong byte size for AppID: %d, should be %d", len(appIDBytes), AppPublicKeySize)
 	}
-	appSecretBytes, err := base64.StdEncoding.DecodeString(this.AppSecret)
+	appSecretBytes, err := base64.StdEncoding.DecodeString(cfg.AppSecret)
 	if err != nil {
-		return nil, fmt.Errorf("unable to decode AppSecret '%s', should be a valid base64 string", this.AppSecret)
+		return nil, fmt.Errorf("unable to decode AppSecret '%s', should be a valid base64 string", cfg.AppSecret)
 	}
 	if len(appSecretBytes) != AppSecretSize {
 		return nil, fmt.Errorf("wrong byte size for AppSecret: %d, should be %d", len(appSecretBytes), AppSecretSize)
