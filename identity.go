a4e29ae67e2b9132edf37a1452b3e17da444e1a7
Michel Levieux <michel.levieux@tanker.io>
2021-09-20 10:12:15 +0200

    merge generate.go into identity.go and corresponding files

--- identity.go
+++ identity.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"encoding/base64"
 	"errors"
+	"github.com/TankerHQ/identity-go/v3/internal/base64_json"
 	"github.com/TankerHQ/identity-go/v3/internal/crypto"
+	"github.com/iancoleman/orderedmap"
 	"golang.org/x/crypto/blake2b"
 
 	"golang.org/x/crypto/ed25519"
@@ -35,6 +37,102 @@ type provisionalIdentity struct {
 	PrivateSignatureKey  []byte `json:"private_signature_key"`
 	PrivateEncryptionKey []byte `json:"private_encryption_key"`
 }
+
+
+func Create(config Config, userID string) (*string, error) {
+	conf, err := config.fromB64()
+	if err != nil {
+		return nil, err
+	}
+	identity, err := generateIdentity(*conf, userID)
+	if err != nil {
+		return nil, err
+	}
+	return base64_json.Encode(identity)
+}
+
+func CreateProvisional(config Config, target string, value string) (*string, error) {
+	conf, err := config.fromB64()
+	if err != nil {
+		return nil, err
+	}
+
+	if target != "email" && target != "phone_number" {
+		return nil, errors.New("unsupported provisional identity target")
+	}
+
+	identity, err := generateProvisionalIdentity(*conf, target, value)
+	if err != nil {
+		return nil, err
+	}
+	return base64_json.Encode(identity)
+}
+
+func GetPublicIdentity(b64Identity string) (*string, error) {
+	type anyPublicIdentity struct {
+		publicIdentity
+		PublicSignatureKey  []byte `json:"public_signature_key,omitempty"`
+		PublicEncryptionKey []byte `json:"public_encryption_key,omitempty"`
+	}
+	publicIdentity := &anyPublicIdentity{}
+	err := base64_json.Decode(b64Identity, publicIdentity)
+	if err != nil {
+		return nil, err
+	}
+
+	if publicIdentity.Target != "user" &&
+		publicIdentity.Target != "email" &&
+		publicIdentity.Target != "phone_number" {
+		return nil, errors.New("unsupported identity target")
+	}
+
+	if publicIdentity.Target != "user" {
+		if publicIdentity.Target == "email" {
+			publicIdentity.Value = hashProvisionalIdentityEmail(publicIdentity.Value)
+		} else {
+			privateIdentity := orderedmap.New()
+			err := base64_json.Decode(b64Identity, &privateIdentity)
+			if err != nil {
+				return nil, err
+			}
+			privateSignatureKey, found := privateIdentity.Get("private_signature_key")
+			if !found {
+				return nil, errors.New("invalid tanker identity")
+			}
+			publicIdentity.Value = hashProvisionalIdentityValue(publicIdentity.Value, privateSignatureKey.(string))
+		}
+		publicIdentity.Target = "hashed_" + publicIdentity.Target
+	}
+
+	return base64_json.Encode(publicIdentity)
+}
+
+func UpgradeIdentity(b64Identity string) (*string, error) {
+	identity := orderedmap.New()
+	err := base64_json.Decode(b64Identity, &identity)
+	if err != nil {
+		return nil, err
+	}
+
+	_, isPrivate := identity.Get("private_encryption_key")
+	target, found := identity.Get("target")
+	if !found {
+		return nil, errors.New("invalid provisional identity (missing target field)")
+	}
+	if target == "email" && !isPrivate {
+		identity.Set("target", "hashed_email")
+		value, valueFound := identity.Get("value")
+		if !valueFound {
+			return nil, errors.New("unsupported identity without value")
+		}
+
+		hashedEmail := blake2b.Sum256([]byte(value.(string)))
+		identity.Set("value", base64.StdEncoding.EncodeToString(hashedEmail[:]))
+	}
+
+	return base64_json.Encode(identity)
+}
+
 
 func generateIdentity(config config, userIDString string) (*identity, error) {
 	generatedAppID := generateAppID(config.AppSecret)
