16183cf5a4f46bfbfd3e7a6d1b98e7f4c7f8829a
Michel Levieux <michel.levieux@tanker.io>
2021-09-24 11:56:42 +0200

    feat: added benchmarks for exported functions

---
+++ internal/app/app_secret_bench_test.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"crypto/rand"
+	"testing"
+)
+
+func BenchmarkGetAppId(b *testing.B) {
+	appSecret := make([]byte, AppSecretSize)
+	rand.Read(appSecret)
+
+	for i := 0 ; i < b.N ; i++ {
+		GetAppId(appSecret)
+	}
+}
