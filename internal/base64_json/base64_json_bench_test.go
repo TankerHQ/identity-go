20a64b49de68d4dfbd8f593374097904d5729481
Michel Levieux <michel.levieux@tanker.io>
2021-09-24 11:56:42 +0200

    feat: added benchmarks for exported functions

---
+++ internal/base64_json/base64_json_bench_test.go
@@ -0,0 +1,71 @@
+package base64_json_test
+
+import (
+	"github.com/TankerHQ/identity-go/v3/internal/base64_json"
+	"github.com/iancoleman/orderedmap"
+	"testing"
+)
+
+type benchStruct struct {
+	PublicSignatureKey string `json:"public_signature_key"`
+	PublicEncryptionKey string `json:"public_encryption_key"`
+	PrivateEncryptionKey string `json:"private_encryption_key"`
+}
+
+var (
+	benchStructValue = benchStruct{
+		PublicSignatureKey:   "str",
+		PublicEncryptionKey:  "str",
+		PrivateEncryptionKey: "str",
+	}
+
+	benchMap = map[string]string{
+		"public_signature_key": "str",
+		"public_encryption_key": "str",
+		"private_encryption_key": "str",
+	}
+
+	benchOrderedMap = func() *orderedmap.OrderedMap {
+		ordered := orderedmap.New()
+		for k, v := range benchMap {
+			ordered.Set(k, v)
+		}
+		return ordered
+	}()
+
+	benchVecs = []struct{
+		desc string
+		data interface{}
+	}{
+		{
+			desc: "Struct",
+			data: benchStructValue,
+		},
+		{
+			desc: "Map",
+			data: benchMap,
+		},
+		{
+			desc: "OrderedMap",
+			data: benchOrderedMap,
+		},
+	}
+)
+
+func BenchmarkEncode(b *testing.B) {
+	for _, vec := range benchVecs {
+		b.Run(vec.desc, func(b *testing.B) {
+			for i := 0 ; i < b.N ; i++ {
+				base64_json.Encode(vec.data)
+			}
+		})
+	}
+}
+
+func BenchmarkDecode(b *testing.B) {
+	encoded, _ := base64_json.Encode(benchStructValue)
+	into := make(map[string]interface{})
+	for i := 0 ; i < b.N ; i++ {
+		base64_json.Decode(*encoded, &into)
+	}
+}
---
+++ internal/crypto/crypto_bench_test.go
@@ -0,0 +1,12 @@
+package crypto_test
+
+import (
+	"github.com/TankerHQ/identity-go/v3/internal/crypto"
+	"testing"
+)
+
+func BenchmarkNewKeyPair(b *testing.B) {
+	for i := 0 ; i < b.N ; i++ {
+		crypto.NewKeyPair()
+	}
+}
