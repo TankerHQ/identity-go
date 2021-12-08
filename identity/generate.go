63cb85d012d054dacb218e9ce272e2cff10288cd
Michel Levieux <michel.levieux@tanker.io>
2021-12-08 15:58:11 +0100

    changed former functions for the mapping to keep working

--- identity/generate.go
+++ identity/generate.go
@@ -4,28 +4,32 @@ import (
 	identity2 "github.com/TankerHQ/identity-go/v3"
 )
 
-// DEPRECATED: use github.com/TankerHQ/identity-go/v3
+// DEPRECATED: use github.com/TankerHQ/identity-go/v3's New function
 func Create(config Config, userID string) (*string, error) {
-	return identity2.Create(identity2.Config{
+	identity, err := identity2.New(identity2.Config{
 		AppID:     config.AppID,
 		AppSecret: config.AppSecret,
 	}, userID)
+	return &identity, err
 }
 
-// DEPRECATED: use github.com/TankerHQ/identity-go/v3
+// DEPRECATED: use github.com/TankerHQ/identity-go/v3's NewProvisional function
 func CreateProvisional(config Config, target string, value string) (*string, error) {
-	return identity2.CreateProvisional(identity2.Config{
+	identity, err := identity2.NewProvisional(identity2.Config{
 		AppID:     config.AppID,
 		AppSecret: config.AppSecret,
-	}, target, value)
+	}, identity2.Target(target), value)
+	return &identity, err
 }
 
 // DEPRECATED: use github.com/TankerHQ/identity-go/v3
 func GetPublicIdentity(b64Identity string) (*string, error) {
-	return identity2.GetPublicIdentity(b64Identity)
+	identity, err := identity2.GetPublicIdentity(b64Identity)
+	return &identity, err
 }
 
 // DEPRECATED: use github.com/TankerHQ/identity-go/v3
 func UpgradeIdentity(b64Identity string) (*string, error) {
-	return identity2.UpgradeIdentity(b64Identity)
+	identity, err := identity2.UpgradeIdentity(b64Identity)
+	return &identity, err
 }
